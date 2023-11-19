0660afb2abebfe353dbe258696859de7e5f51310
rayanzak <rayan.hassici@ensia.edu.dz>
2023-11-19 02:05:18 +0100

    finished part 2

--- supabase-react/src/Account.jsx
+++ supabase-react/src/Account.jsx
@@ -1,107 +1,105 @@
-import { useState, useEffect } from "react";
-import { supabase } from "./supabaseClient";
+import { useState, useEffect } from 'react'
+import { supabase } from './supabaseClient'
 
-export default function Account() {
-  const [loading, setLoading] = useState(true);
-  const [full_name, setFullName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [id, setId] = useState(null);
-  const [username, setUsername] = useState(null);
-  const [avatar_url, setAvatarUrl] = useState(null);
+export default function Account({ session }) {
+  const [loading, setLoading] = useState(true)
+  const [username, setUsername] = useState(null)
+  const [full_name, setFullName] = useState(null)
+  const [avatar_url, setAvatarUrl] = useState(null)
 
   useEffect(() => {
-    let ignore = false;
+    let ignore = false
     async function getProfile() {
-      setLoading(true);
+      setLoading(true)
+      const { user } = session
 
       const { data, error } = await supabase
-        .from("accounts")
-        .select(`*`)
-        .single();
+        .from('profiles')
+        .select(`username, full_name, avatar_url`)
+        .eq('id', user.id)
+        .single()
 
-        console.log(data);
       if (!ignore) {
         if (error) {
-          console.warn(error);
+          console.warn(error)
         } else if (data) {
+          setUsername(data.username)
           setFullName(data.full_name)
-          setEmail(data.email);
-          setId(data.id);
-          setUsername(data.username);
-          setAvatarUrl(data.avatar_url);
+          setAvatarUrl(data.avatar_url)
         }
       }
 
-      setLoading(false);
+      setLoading(false)
     }
 
-    getProfile();
+    getProfile()
 
     return () => {
-      ignore = true;
-    };
-  }, []);
+      ignore = true
+    }
+  }, [session])
 
   async function updateProfile(event, avatarUrl) {
-    event.preventDefault();
+    event.preventDefault()
 
-    setLoading(true);
+    setLoading(true)
+    const { user } = session
 
     const updates = {
-      id,
-      full_name,
+      id: user.id,
       username,
+      full_name,
       avatar_url,
-      email,
       updated_at: new Date(),
-    };
+    }
 
-    const { error } = await supabase.from("accounts").upsert(updates);
+    const { error } = await supabase.from('profiles').upsert(updates)
 
     if (error) {
-      alert(error.message);
+      alert(error.message)
     } else {
-      setAvatarUrl(avatarUrl);
+      setAvatarUrl(avatarUrl)
     }
-    setLoading(false);
+    setLoading(false)
   }
 
   return (
     <form onSubmit={updateProfile} className="form-widget">
       <div>
         <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={email || ""} disabled />
+        <input id="email" type="text" value={session.user.email} disabled />
       </div>
       <div>
-        <label htmlFor="username">Username</label>
+        <label htmlFor="username">Name</label>
         <input
           id="username"
           type="text"
           required
-          value={username || ""}
+          value={username || ''}
           onChange={(e) => setUsername(e.target.value)}
         />
       </div>
       <div>
-      <label htmlFor="fullname">Full Name</label>
+        <label htmlFor="full_name">Full Name</label>
         <input
-          id="fullname"
+          id="full_name"
           type="text"
-          required
-          value={full_name || ""}
+          value={full_name || ''}
           onChange={(e) => setFullName(e.target.value)}
         />
       </div>
+
       <div>
-        <button
-          className="button block primary"
-          type="submit"
-          disabled={loading}
-        >
-          {loading ? "Loading ..." : "Update"}
+        <button className="button block primary" type="submit" disabled={loading}>
+          {loading ? 'Loading ...' : 'Update'}
         </button>
       </div>
 
+      <div>
+        <button className="button block" type="button" onClick={() => supabase.auth.signOut()}>
+          Sign Out
+        </button>
+      </div>
     </form>
-  );
+  )
 }
