ec397ac12c50a65232c9b28b32512b9193050e13
rayanzak <rayan.hassici@ensia.edu.dz>
2023-11-24 15:01:46 +0100

    added etc logo

--- supabase-react/src/App.jsx
+++ supabase-react/src/App.jsx
@@ -1,27 +1,37 @@
-import './App.css'
-import { useState, useEffect } from 'react'
-import { supabase } from './supabaseClient'
-import Auth from './Auth'
-import Account from './Account'
+import "./App.css";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+import Auth from "./Auth";
+import Account from "./Account";
+import ETCLogo from "./assets/etc_logo.svg";
 
 function App() {
-  const [session, setSession] = useState(null)
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
 
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
-    </div>
-  )
+    <>
+      <div className="w-full flex flex-start">
+        <img src={ETCLogo} width={150} />
+      </div>
+      <div className="container" style={{ padding: "50px 0 100px 0" }}>
+        {!session ? (
+          <Auth />
+        ) : (
+          <Account key={session.user.id} session={session} />
+        )}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
